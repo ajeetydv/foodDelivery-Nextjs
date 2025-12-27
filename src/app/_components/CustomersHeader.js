06c9e19ac6f24e3739206933f9a955e619fb00aa
ajeet <jeetay.yadav284@gmail.com>
2025-12-27 15:10:59 +0530

    all changes

--- src/app/_components/CustomersHeader.js
+++ src/app/_components/CustomersHeader.js
@@ -1,111 +1,141 @@
-'use client'
+'use client';
 
-import Link from 'next/link'
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
- const CustomersHeader = (props) => {
-    const router = useRouter();
-   const userStorage = localStorage.getItem('user') && JSON.parse(localStorage.getItem('user'));
-    const cartStorage = localStorage.getItem('cart') && JSON.parse(localStorage.getItem('cart'));
-   const[cartNumber, setCartNumber] = useState(cartStorage?.length);
-   const[cartItem, setCartItem] = useState(cartStorage);
-   
-   
-   useEffect(()=>{
-    
-      if(props.cartData){
-         console.log(props)
-      if (cartNumber){
-         if(cartItem[0].resto_id !=props.cartData.resto_id){
-             localStorage.removeItem('cart');
-             setCartNumber(1)
-             setCartItem([props.cartData])
-             localStorage.setItem('cart', JSON.stringify([props.cartData]))
-             alert("You are taking order from other resto")
-             
-         }
-         
-         else{
-          let localCartItem = cartItem
-          localCartItem.push(JSON.parse(JSON.stringify(props.cartData)))
-          setCartItem(localCartItem);
-          setCartNumber(cartNumber+1)
-          localStorage.setItem('cart', JSON.stringify(localCartItem))
-         }
-      }
-      else{
-         setCartNumber(1);
-         setCartItem([props.cartData])
-       localStorage.setItem('cart', JSON.stringify([props.cartData]))
-      }
-    
-   }
+const CustomersHeader = ({ cartData, removeCartData }) => {
+  const router = useRouter();
 
-   },[props.cartData])
+  const [user, setUser] = useState(null);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
 
-   useEffect(() => {
-        if (props.removeCartData) {
-            let localCartItem = cartItem.filter((item) => {
-                return item._id != props.removeCartData
-            });
-            setCartItem(localCartItem);
-            setCartNumber(cartNumber - 1);
-            localStorage.setItem('cart', JSON.stringify(localCartItem))
-            if (localCartItem.length == 0) {
-                localStorage.removeItem('cart')
-            }
-        }
-    }, [props.removeCartData])
+  /* -------------------- Load from localStorage (client only) -------------------- */
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
 
-    useEffect(()=>{
+    const storedUser = localStorage.getItem('user');
+    const storedCart = localStorage.getItem('cart');
 
-        if(props.removeCartData){
-            setCartItem([])
-            setCartNumber(0);
-            localStorage.removeItem('cart');
-        }
-
-    },[props.removeCartData])
-
-    const logout = ()=>{
-       localStorage.removeItem('user')
-       router.push('/')
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
     }
 
+    if (storedCart) {
+      const parsedCart = JSON.parse(storedCart);
+      setCartItems(parsedCart);
+      setCartCount(parsedCart.length);
+    }
+  }, []);
+
+  /* -------------------- Add to Cart -------------------- */
+  useEffect(() => {
+    if (!cartData) return;
+
+    setCartItems((prev) => {
+      let updatedCart = [];
+
+      if (prev.length > 0 && prev[0].resto_id !== cartData.resto_id) {
+        alert('You are taking order from another restaurant');
+        updatedCart = [cartData];
+      } else {
+        updatedCart = [...prev, cartData];
+      }
+
+      localStorage.setItem('cart', JSON.stringify(updatedCart));
+      setCartCount(updatedCart.length);
+
+      return updatedCart;
+    });
+  }, [cartData]);
+
+  /* -------------------- Remove Item from Cart -------------------- */
+  useEffect(() => {
+    if (!removeCartData) return;
+
+    setCartItems((prev) => {
+      const updatedCart = prev.filter(
+        (item) => item._id !== removeCartData
+      );
+
+      if (updatedCart.length === 0) {
+        localStorage.removeItem('cart');
+      } else {
+        localStorage.setItem('cart', JSON.stringify(updatedCart));
+      }
+
+      setCartCount(updatedCart.length);
+      return updatedCart;
+    });
+  }, [removeCartData]);
+
+  /* -------------------- Logout -------------------- */
+  const logout = () => {
+    localStorage.removeItem('user');
+    router.push('/');
+  };
+
+  /* -------------------- JSX -------------------- */
   return (
-    <>
-    <div className='blackpart'>
-             <div className='logo-left'>
-                <img src='/burger-solid.svg' /> 
-             </div>
+    <div className="blackpart">
+      <div className="logo-left">
+        <img src="/burger-solid.svg" alt="Logo" />
+      </div>
 
-            
-              <ul className="fdbar-nav cstm">
-                 <li><Link  href="/"><img src='/home.svg' /></Link></li> 
-                 { userStorage? 
-                 <>
-                 
-                  <li><Link href="/profile"><img src="/user-circle-solid.svg" /> <b>HI {userStorage?.name}</b></Link></li>
-                 <li  onClick={logout}><img src='/power-off-solid.svg' /> <b >LOGOUT</b> </li>  
-                 </>
-                
-                 :<li><Link href="/user"><img src='/power-off-solid.svg' /> <b>LOGIN</b> </Link></li>
-                 }
-          
-                 <li className='cart-mgs'><Link href={cartNumber?"/cart":"#"}><img src='/cart-ic.svg' /><span>{(cartNumber? cartNumber: 0)}</span></Link></li> 
-                 <li>
-                    <Link href="/" ><img src='/utensils-solid.svg' /> <b> Add Restaurant</b></Link>
-                </li>
-                <li className='cart-mgs'>
-                    <Link href="/deliverypartner" ><img src='/handshake-solid.svg' /> <b>Delivery Partner</b></Link>
-                </li> 
-             </ul>
+      <ul className="fdbar-nav cstm">
+        <li>
+          <Link href="/">
+            <img src="/home.svg" alt="Home" />
+          </Link>
+        </li>
+
+        {user ? (
+          <>
+            <li>
+              <Link href="/profile">
+                <img src="/user-circle-solid.svg" alt="Profile" />
+                <b> HI {user.name}</b>
+              </Link>
+            </li>
+
+            <li onClick={logout}>
+              <img src="/power-off-solid.svg" alt="Logout" />
+              <b> LOGOUT</b>
+            </li>
+          </>
+        ) : (
+          <li>
+            <Link href="/user">
+              <img src="/power-off-solid.svg" alt="Login" />
+              <b> LOGIN</b>
+            </Link>
+          </li>
+        )}
+
+        <li className="cart-mgs">
+          <Link href={cartCount ? '/cart' : '#'}>
+            <img src="/cart-ic.svg" alt="Cart" />
+            <span>{cartCount}</span>
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/">
+            <img src="/utensils-solid.svg" alt="Add Restaurant" />
+            <b> Add Restaurant</b>
+          </Link>
+        </li>
+
+        <li className="cart-mgs">
+          <Link href="/deliverypartner">
+            <img src="/handshake-solid.svg" alt="Delivery Partner" />
+            <b> Delivery Partner</b>
+          </Link>
+        </li>
+      </ul>
     </div>
+  );
+};
 
-    </>
-  )
-}
-
-
-export default CustomersHeader
+export default CustomersHeader;
